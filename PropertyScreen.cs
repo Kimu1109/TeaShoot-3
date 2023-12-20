d293f6d0a214ee4cf13d1f688569dd2725475f9f
Kimu1109 <118643872+Kimu1109@users.noreply.github.com>
2023-12-20 21:06:13 +0900

    プロジェクト ファイルを追加します。

---
+++ PropertyScreen.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static TeaShoot_3.Program;
+using static TeaShoot_3.obj;
+using static DxLibDLL.DX;
+using Microsoft.VisualBasic;
+
+namespace TeaShoot_3
+{
+    public partial class PropertyScreen : Form
+    {
+
+        public PropertyScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void propertyGrid1_Resize(object sender, EventArgs e)
+        {
+        }
+
+        private void PropertyScreen_Resize(object sender, EventArgs e)
+        {
+            propertyGrid1.Width = this.Width - 15;
+            propertyGrid1.Height = this.Height - 15 - 15 - this.Top;
+        }
+        public void SetPsObj(obj o)
+        {
+            propertyGrid1.SelectedObject = o;
+        }
+        private void PropertyScreen_Load(object sender, EventArgs e)
+        {
+            //ListView1のすべての列を自動調節
+            foreach (ColumnHeader ch in listView1.Columns)
+            {
+                ch.Width = -2;
+            }
+            ReloadResist();
+            DrawResist();
+        }
+        private void DrawResist()
+        {
+            listView1.Items.Clear();
+            foreach(var l in resistList)
+            {
+                var li = new ListViewItem(l.num.ToString());
+                li.SubItems.Add(l.text);
+                listView1.Items.Add(li);
+            }
+        }
+        public void propertyToolStripMenuItem_Click()
+        {
+            propertyGrid1.BringToFront();
+        }
+
+        public void registerToolStripMenuItem_Click()
+        {
+            DrawResist();
+            listView1.BringToFront();
+        }
+
+        public void 追加ToolStripMenuItem_Click()
+        {
+            resistList.Add(new obj(resistList.Count, obj.ObjType.Enemy, text: "(;-;)"));
+            DrawResist();
+        }
+
+        private void objectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void 保存ToolStripMenuItem_Click()
+        {
+            foreach(var obj in resistList)
+            {
+                obj.WriteObj(obj, obj.AppPath() + @"\resist\" + obj.num + ".dat");
+            }
+            MessageBox.Show("finished!");
+        }
+
+        public void 再読み込みToolStripMenuItem_Click()
+        {
+            ReloadResist();
+            DrawResist();
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if(listView1.SelectedIndices.Count <= 0) { return; }
+            propertyGrid1.SelectedObject = resistList[listView1.SelectedIndices[0]];
+            propertyGrid1.BringToFront();
+        }
+
+        private void propertyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var op = obj.Clone(objList[0]);
+            player = op;
+            objList.Clear();
+            objList.Add(op);
+
+            int mapX = 0;
+            int fontHeight = GetFontSize();
+            using (var sr = new StreamReader(AppPath() + @"\map\" + DevFileName))
+            {
+                string st = sr.ReadLine();
+                while (st != null)
+                {
+                    var srArr = st.Split('/');
+
+                    for (int i = 0; i < srArr.Count(); i++)
+                    {
+                        if (Convert.ToInt32(srArr[i]) != 0)
+                        {
+                            var o = obj.Clone(ResistIndexOf(Convert.ToInt32(srArr[i])));
+                            o.y = i * fontHeight;
+                            o.x = mapX * 50;
+                            objList.Add(o);
+                        }
+                    }
+
+                    mapX++;
+                    st = sr.ReadLine();
+                }
+            }
+
+            WaitTimer(500);
+        }
+
+        public void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using(var sw = new StreamWriter(obj.AppPath() + @"\map\map1.dat"))
+            {
+                var writeList = new List<int[]>();
+                int nowSpace = 0;
+                int fontHeight = GetFontSize();
+                int maxX = -1;
+
+                foreach(var orz in objList)
+                {
+                    if(orz.x > maxX)
+                    {
+                        maxX = (int)orz.x;
+                    }
+                    if (orz.y < 0) orz.y = 0;
+                    if (orz.y > 480) orz.y = 480;
+                }
+
+                while (nowSpace * 50 <= maxX)
+                {
+                    var YList = new int[480 / fontHeight + 1];
+                    foreach(var o in objList)
+                    {
+                        if(o.num != 0)
+                        {
+                            if ((int)o.x == nowSpace * 50)
+                            {
+                                YList[(int)o.y / fontHeight] = o.num;
+                            }
+                        }
+                    }
+                    writeList.Add(YList);
+
+                    nowSpace += 1;
+                }
+                foreach(var i in writeList)
+                {
+                    string writeStr = "";
+                    foreach (var n in i)
+                    {
+                        writeStr += n.ToString() + "/";
+                    }
+                    writeStr = writeStr.TrimEnd('/');
+                    sw.WriteLine(writeStr);
+                }
+            }
+            MessageBox.Show("finished writing map1.dat.");
+        }
+
+        public void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isDevelop = false;
+            player.FitText("_-=三(^o^)_旦");
+        }
+        public int GetListViewIndexNum()
+        {
+            if(listView1.SelectedIndices.Count == 0) { return 0; }
+            return resistList[listView1.SelectedIndices[0]].num;
+        }
+
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void devFileNameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DevFileName = Interaction.InputBox("ファイル名を入力");
+        }
+
+        private void テキストを設定ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count == 0) return;
+
+            using(var n = new TextDialog())
+            {
+                n.text = resistList[listView1.SelectedIndices[0]].text;
+                if (n.ShowDialog() == DialogResult.OK)
+                {
+                    resistList[listView1.SelectedIndices[0]].FitText(n.text);
+                    DrawResist();
+                }
+            }
+
+        }
+
+        private void テキストフィットToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count == 0) return;
+            resistList[listView1.SelectedIndices[0]].FitText(resistList[listView1.SelectedIndices[0]].text);
+        }
+    }
+}
---
+++ bosses.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TeaShoot_3.obj;
+using static DxLibDLL.DX;
+using Microsoft.VisualBasic;
+
+namespace TeaShoot_3
+{
+    public static class bosses
+    {
+        public static void ProcessBoss1(int i)
+        {
+            var b1 = objList[i];
+
+            if (b1.boss1 == null)
+            {
+                b1.boss1 = new boss1();
+                b1.boss1.attack = boss1.attackType.MoveFirst;
+            }
+
+            var b1b = b1.boss1;
+
+            switch (b1b.attack)
+            {
+                case boss1.attackType.MoveFirst:
+
+                    if((int)b1.x == 640 - b1.width && (int)b1.y == 480 - b1.height / 2)
+                    {
+                        b1b.nextAttack();
+                    }
+                    double angle = Math.Atan2(b1.x - (640 - b1.width), b1.y - (480 - b1.height / 2));
+
+                    b1.x += (float)Math.Cos(angle);
+                    b1.y += (float)Math.Sin(angle);
+
+                    Console.WriteLine(b1.x.ToString() + "," + b1.y.ToString());
+
+                    break;
+                case boss1.attackType.ShieldWall:
+                    b1b.attackWait++;
+                    if(b1b.attackWait > 50)
+                    {
+                        b1b.attackWait = 0;
+
+                        b1b.y++;
+                        if (b1b.y >= 480 / player.height) b1b.y = 0;
+
+                        for(int z = 0; z < 480 / player.height; z++)
+                        {
+                            if(b1b.y != z)
+                            {
+                                var ao = ResistIndexOf(14);
+
+                                ao.x = 640;
+                                ao.y = z * player.height;
+
+                                objList.Add(ao);
+                            }
+                        }
+
+                        b1b.attackNum++;
+                        if(b1b.attackNum > 20)
+                        {
+                            b1b.nextAttack();
+                        }
+                    }
+                    break;
+            }
+
+        }
+
+
+    }
+    [Serializable]
+    public class boss1
+    {
+        public attackType attack;
+        public int attackWait;
+        public int attackNum;
+        public bool isRandom;
+
+        public int x;
+        public int y;
+
+        public enum attackType
+        {
+            MoveFirst = 0,
+            ShieldWall = 1,
+            CoffeeRain = 2,
+            CoffeeBeam = 3,
+            CoffeeBound = 4,
+            CoffeeMore = 5,
+            CoffeeSide = 6,
+            CoffeeCycle = 7,
+            CoffeeFire = 8,
+            MoveLast = 9
+        }
+        public void nextAttack()
+        {
+            if(!isRandom)
+            {
+                if(attack == attackType.CoffeeFire) 
+                {
+                    isRandom = true;
+                }
+                else
+                {
+                    attack = (attackType)((int)attack + 1);
+                    attackNum = 0;
+                    attackWait = 0;
+                }
+            }
+            if (isRandom)
+            {
+                attack = (attackType)obj.rnd.Next((int)attackType.ShieldWall, (int)attackType.CoffeeFire);
+                attackNum = 0;
+                attackWait = 0;
+            }
+        }
+    }
+}
